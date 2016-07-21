fd8c0dcc8c639fc6d935c60a83990e9baeb4a456
DhanaLakshmiRamachandran <dhanadivya17@gmail.com>
2016-07-21 19:42:54 +0530

    Create rev.java

---
+++ rev.java
@@ -0,0 +1,19 @@
+public class Reverse {
+	public static void main(String[] args)
+
+	{
+		// TODO Auto-generated method stub
+		String str="hello";
+		String rev="";
+		for(int i=str.length()-1;i>=0;i--)
+		{
+			rev=rev+str.charAt(i);
+		}
+		
+		System.out.println("reversed string:"+rev);
+	}
+}
+
+	
+
+
